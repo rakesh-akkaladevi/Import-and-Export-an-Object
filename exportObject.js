fa3d12cca064bbb71ee0429d9c7b39371460ade9
rakesh-akkaladevi <rakeshakkaladevi2011@gmail.com>
2022-04-30 14:59:43 +0000

    committed

---
+++ exportObject.js
@@ -0,0 +1,8 @@
+// Write an object with keys as `firstName` and `lastName` and values as `John` and
+// `Wilson` respectively in the file
+//  `exportObject.js` and export it using the default export syntax.
+
+module.exports = name = {
+  firstName: "John",
+  lastName: "Wilson",
+};
